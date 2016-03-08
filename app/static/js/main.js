5976ec5187e769ab85706b00af0d7c91440cff4f
ID3055 <ID3055@qq.com>
2016-03-08 20:52:32 +0800

    修改用户头像处js效果

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,15 +1,12 @@
 ﻿$(function(){ 
   $(".nav-main li").hover(function(){
-    /*下拉框出现*/
     $("ul",this).slideDown(300)
   },function(){
-    /*下拉框消失*/
     $("ul",this).hide();
     });
 }); 
-$(function(){ 
+/*$(function(){ 
   $(".i_user").hover(function(){
-    /*下拉框出现*/
     $("img",this).animate({top:'12px',
                             width:'66px',
                             height:'66px',
@@ -17,7 +14,6 @@ $(function(){
                             },'200');
     $(".i_menu",this).slideDown(500);
   },function(){
-    /*下拉框消失*/
     $("img",this).animate({top:'0px',
                             width:'40px',
                             height:'40px',
@@ -26,3 +22,45 @@ $(function(){
     $(".i_menu",this).hide();
     });
 }); 
+*/
+
+// 线程 IDs
+var mouseover_tid = [];
+var mouseout_tid = [];
+ 
+$(function(){
+        $('.i_user').hover(
+            // 鼠标进入
+            function(){
+                var _self = this;
+                // 停止卷起事件
+                clearTimeout(mouseout_tid[0]);
+                // 当鼠标进入超过 0.2 秒, 展开菜单, 并记录到线程 ID 中
+                mouseover_tid[0] = setTimeout(function() {
+                    $("img",_self).animate({top:'12px',
+                            width:'66px',
+                            height:'66px',
+                            left:'-13px',
+                            },'200');
+                    $(".i_menu",_self).slideDown(150);
+                }, 400);
+            },
+ 
+            // 鼠标离开
+            function(){
+                var _self = this;
+                // 停止展开事件
+                clearTimeout(mouseover_tid[0]);
+                // 当鼠标离开超过 0.2 秒, 卷起菜单, 并记录到线程 ID 中
+                mouseout_tid[0] = setTimeout(function() {
+                    $("img",_self).animate({top:'0px',
+                            width:'40px',
+                            height:'40px',
+                            left:'0px',
+                            border:'2px',
+                            },'200');
+                    $(".i_menu",_self).slideUp(150);
+                }, 400);
+            }
+        );
+});
